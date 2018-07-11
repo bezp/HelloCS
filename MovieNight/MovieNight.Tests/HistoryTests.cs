0f278e9821a843c6fff10938169ed05e0e4f8186
bezalel <bezalelp@uci.edu>
2018-07-11 15:35:54 -0400

    added tests for HistoryTest

--- MovieNight/MovieNight.Tests/HistoryTests.cs
+++ MovieNight/MovieNight.Tests/HistoryTests.cs
@@ -18,10 +18,18 @@ namespace MovieNight.Tests
             Assert.True(expected == actual.GetType());
             //Assert.True(actual.GetType() == expected);
         }
-        //public HistoryTests()
-        //{
 
-        //}
+        public void Test_HistoryMovies()
+        {
+            var expected = 1;
+            //subject under test
+            var sut = new History(); //want it to giv u the list of movies youve seen
+            var actual = sut.Movies;
+
+            Assert.True(typeof(List<Movie>) == actual.GetType()); //needs to be a list
+            Assert.True(expected <= actual.Count);
+        }
+
 
     }
 }
